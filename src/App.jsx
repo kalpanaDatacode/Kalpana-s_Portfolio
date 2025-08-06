cc63540cc649e894e0db9a632d4ee24c19f1209f
Kalpana Patidar <kalpanamds1105@gmail.com>
2025-08-06 12:28:10 +0530

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Services from './components/Services';
 import Projects from './components/Projects';
-// import Testimonials from './components/Testimonials';
+import Testimonials from './components/Testimonials';
 
 
 
@@ -17,7 +17,7 @@ function App(){
            <About/>
            <Services/>
            <Projects/>
-           {/* < Testimonials/> */}
+           < Testimonials/>
         </>
    )
 }
