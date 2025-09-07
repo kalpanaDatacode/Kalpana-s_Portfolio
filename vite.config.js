495f6bf83f67677da409056368f58031b50beae6
KalpanaDatacode <kalpana.datacode@gmail.com>
2025-09-07 21:58:25 +0530

    Update Vite base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Kalpana's_Portfolio/", // 👈 your repo name
+  base: "/Kalpana_Portfolio/", // 👈 your repo name
 })
